fe7f7a884d8a261968ca8e00d72bda27d7b8061a
stenzel65 <60339754+stenzel65@users.noreply.github.com>
2020-01-30 09:37:00 +0100

    Update desinDocker.sh

--- desinDocker.sh
+++ desinDocker.sh
@@ -5,3 +5,9 @@ apt-get purge docker-ce
 
 #Supprime images, conteneurs et volumes docker
 rm -rf /var/lib/docker
+
+#installation du packet pour utiliser pip
+sudo apt install python-pip
+
+#desinstalle docker-compose
+pip uninstall docker-compose
