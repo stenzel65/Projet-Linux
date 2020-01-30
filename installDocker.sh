30f28b737748a65711409f386fdd942773f1b951
stenzel65 <60339754+stenzel65@users.noreply.github.com>
2020-01-30 09:13:01 +0100

    Update installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
 
+#mets à jour la base de donnée locale des logiciels
 apt-get update
 
+#supprime les éventuelles versions de docker plus anciennes
 apt-get remove docker docker-engine docker.io
 
+#installe docker
 apt install docker.io
 
+#paramétrage de docker pour démarrer au démarrage du système
 systemctl start docker
-
 systemctl enable docker
+
+#vérifie la version de docker
+docker --version
