7577fbc9e31fb1859b01d03385cf8e91a5bb7b70
stenzel65 <60339754+stenzel65@users.noreply.github.com>
2020-07-10 20:05:22 +0200

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,5 +1,17 @@
+#Mise à jour des paquets existants
+
+apt-get update
+
 #Installation des packages nécessaires
 
-apt-get install mailutils
-apt-get install postfix
+apt-get install logwatch
+apt-get install ssmtp mailx
 
+#Envoie du mail
+
+logwatch -mailto juliensten@live.fr
+
+#Ajout dans cron pour avoir tout les matins
+
+sudo crontab -e
+0 8 * * * logwatch -mailto juliensten@live.fr
